16a81a82d3c55e49f902bac6bbd29cdd401e4c98
Maqsood <maqsoodansari818@gmail.com>
2021-11-25 22:13:59 +0530

    local

---
+++ src/main/java/com/maq/EurekaZuulService2Application.java
@@ -0,0 +1,19 @@
+package com.maq;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaZuulService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaZuulService2Application.class, args);
+	}
+  
+	@PostConstruct
+	public void test() {
+		System.out.println("test");
+	}
+}
